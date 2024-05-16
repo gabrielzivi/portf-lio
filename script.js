a873f1251730947ed896c590eef6850db9f7d0f2
gabrielzivi <164963381+gabrielzivi@users.noreply.github.com>
2024-05-16 14:03:56 -0300

    fix code

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
 var menu = document.querySelector('#menu-bars');
-
 var header = document.querySelector('header');
 var cursor1 = document.querySelector('.cursor-1');
 var cursor2 = document.querySelector('.cursor-2');
 
-menu.onclick = () =>{
-
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
-menu.onscroll = () =>{
-
+menu.onscroll = () => {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
 
-window.onmousemove = (e) =>{
-
-    cursor1.style.top = e.pageY + 'px';
-    cursor1.style.left = e.pageX + 'px';
-    cursor2.style.top = e.pageY + 'px';
-    cursor2.style.left = e.pageX + 'px';
-}
-
-document.querySelectorAll('a').forEach(links =>{
-
-    links.onmouseenter = () =>{
-
-        cursor1.classList.add('active');
-        cursor2.classList.add('active');
-
-    }
-    links.onmouseleave = () =>{
-
-        cursor1.classList.remove('active');
-        cursor2.classList.remove('active');
-
-    }
-});
 
